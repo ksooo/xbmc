f6b54ddce1212fdbd57e598f2ea63140dc0f84d5
ksooo <kai.sommerfeld@gmx.com>
2025-05-23 22:07:44 +0200

    [utils] SonarQube finding: CEventStream, CSubscription: Replace function pointers with std::function

--- xbmc/cores/RetroPlayer/shaders/ShaderPresetFactory.cpp
+++ xbmc/cores/RetroPlayer/shaders/ShaderPresetFactory.cpp
@@ -27,7 +27,17 @@ CShaderPresetFactory::CShaderPresetFactory(ADDON::CAddonMgr& addons) : m_addons(
 {
   UpdateAddons();
 
-  m_addons.Events().Subscribe(this, &CShaderPresetFactory::OnEvent);
+  m_addons.Events().Subscribe(this,
+                              [this](const ADDON::AddonEvent& event)
+                              {
+                                if (typeid(event) == typeid(ADDON::AddonEvents::Enabled) ||
+                                    typeid(event) == typeid(ADDON::AddonEvents::Disabled) ||
+                                    typeid(event) == typeid(ADDON::AddonEvents::UnInstalled) ||
+                                    typeid(event) == typeid(ADDON::AddonEvents::ReInstalled))
+                                {
+                                  UpdateAddons();
+                                }
+                              });
 }
 
 CShaderPresetFactory::~CShaderPresetFactory()
@@ -84,17 +94,6 @@ bool CShaderPresetFactory::CanLoadPreset(const std::string& presetPath)
     bSuccess = (m_loaders.find(extension) != m_loaders.end());
 
   return bSuccess;
-}
-
-void CShaderPresetFactory::OnEvent(const ADDON::AddonEvent& event)
-{
-  if (typeid(event) == typeid(ADDON::AddonEvents::Enabled) ||
-      typeid(event) == typeid(ADDON::AddonEvents::Disabled) ||
-      typeid(event) == typeid(ADDON::AddonEvents::UnInstalled) ||
-      typeid(event) == typeid(ADDON::AddonEvents::ReInstalled))
-  {
-    UpdateAddons();
-  }
 }
 
 void CShaderPresetFactory::UpdateAddons()
