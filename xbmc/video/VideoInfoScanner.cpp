79322b7ace35eae5476f309dda2c7ce1358ab575
ksooo <kai.sommerfeld@gmx.com>
2025-06-10 14:22:09 +0200

    [FileItem] SonarQube finding: Encapsulate member m_dateTime

--- xbmc/video/VideoInfoScanner.cpp
+++ xbmc/video/VideoInfoScanner.cpp
@@ -2198,15 +2198,16 @@ CVideoInfoScanner::~CVideoInfoScanner()
         // according to ListItem.setInfo() documentation date format should be "d.m.Y"
         if (pItem->m_dwSize)
           digest.Update(std::to_string(pItem->m_dwSize));
-        if (pItem->m_dateTime.IsValid())
-          digest.Update(StringUtils::Format("{:02}.{:02}.{:04}", pItem->m_dateTime.GetDay(),
-                                            pItem->m_dateTime.GetMonth(),
-                                            pItem->m_dateTime.GetYear()));
+
+        const CDateTime& dateTime{pItem->GetDateTime()};
+        if (dateTime.IsValid())
+          digest.Update(StringUtils::Format("{:02}.{:02}.{:04}", dateTime.GetDay(),
+                                            dateTime.GetMonth(), dateTime.GetYear()));
       }
       else
       {
         digest.Update(&pItem->m_dwSize, sizeof(pItem->m_dwSize));
-        KODI::TIME::FileTime time = pItem->m_dateTime;
+        const KODI::TIME::FileTime time = pItem->GetDateTime();
         digest.Update(&time, sizeof(KODI::TIME::FileTime));
       }
       if (IsVideo(*pItem) && !PLAYLIST::IsPlayList(*pItem) && !pItem->IsNFO())
