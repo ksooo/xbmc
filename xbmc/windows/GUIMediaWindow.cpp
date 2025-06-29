14e619e6def0697fc5edb632ee3279f5980a3e5f
ksooo <kai.sommerfeld@gmx.com>
2025-06-30 01:18:26 +0200

    [temp] Workaround video nav window taking ages to open

--- xbmc/windows/GUIMediaWindow.cpp
+++ xbmc/windows/GUIMediaWindow.cpp
@@ -752,8 +752,8 @@ bool CGUIMediaWindow::GetDirectory(const std::string &strDirectory, CFileItemLis
     if (!GetDirectoryItems(pathToUrl, dirItems, UseFileDirectories()))
       return false;
 
-    if (GetID() == WINDOW_VIDEO_NAV)
-      dirItems.ReplaceBlurayFiles();
+//    if (GetID() == WINDOW_VIDEO_NAV)
+//      dirItems.ReplaceBlurayFiles();
 
     // assign fetched directory items
     items.Assign(dirItems);
