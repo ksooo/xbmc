b8b6541b7e7dd3b81e290e3373b902f7b409caef
ksooo <kai.sommerfeld@gmx.com>
2025-11-01 19:51:59 +0100

    [windows][Estuary] GUIMediaWindow::OnInitWindow: Load items async to avoid completely empty screen until item list is filled. Allow window opening animation to run in parallel. MyVideoNav: Avoid screen flickering while loading items."

--- xbmc/windows/GUIMediaWindow.cpp
+++ xbmc/windows/GUIMediaWindow.cpp
@@ -1669,8 +1669,8 @@ void CGUIMediaWindow::OnRenameItem(int iItem)
 
 void CGUIMediaWindow::OnInitWindow()
 {
-  // initial fetch is done unthreaded to ensure the items are setup prior to skin animations kicking off
-  m_backgroundLoad = false;
+//  if (m_guiState)
+//    m_viewControl.SetCurrentView(m_guiState->GetViewAsControl());
 
   // the start directory may change during Refresh
   bool updateStartDirectory = URIUtils::PathEquals(m_vecItems->GetPath(), m_startDirectory, true);
@@ -1697,8 +1697,6 @@ void CGUIMediaWindow::OnInitWindow()
     // reset the history based on the path of the items
     SetHistoryForPath(m_startDirectory);
   }
-
-  m_backgroundLoad = true;
 
   CGUIWindow::OnInitWindow();
 }
